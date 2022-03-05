67d691233e219c85922b1fc82b0822e492a30668
netholi <ajkadakkal@gmail.com>
2022-03-05 11:43:35 +0530

    modularize the code

---
+++ lib/quoteText.dart
@@ -0,0 +1,36 @@
+import 'quote.dart';
+
+List<Quote> quotes = [
+  Quote(
+      text: 'The journey of a thousand miles begins with one step.',
+      author: 'Lao Tzu'),
+  Quote(
+      text: 'When the going gets tough, the tough get going.',
+      author: 'Joe Kennedy'),
+  Quote(text: 'Get busy living or get busy dying.', author: 'Stephen King'),
+  Quote(
+      text: 'You miss 100 percent of the shots you never take..',
+      author: 'Wayne Gretzky'),
+  Quote(
+      text: 'If you’re going through hell, keep going.',
+      author: 'Winston Churchill'),
+  Quote(
+      text:
+          'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.',
+      author: 'Mark Twain'),
+  Quote(
+      text:
+          'Sing like no one’s listening, love like you’ve never been hurt, dance like nobody’s watching, and live like it’s heaven on earth.',
+      author: 'Mark Twain'),
+  Quote(
+      text: 'People are just as happy as they make up their minds to be.',
+      author: 'Abraham Lincoln'),
+  Quote(
+      text: 'The only impossible journey is the one you never begin.',
+      author: 'ony Robbins'),
+  Quote(text: 'Get busy living or get busy dying.', author: 'Stephen King'),
+  Quote(
+      text:
+          'Insanity is doing the same thing over and over again and expecting different results.',
+      author: 'Albert Einstein')
+];
